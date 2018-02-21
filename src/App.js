ddc1eec43a01ed829056ebbdf34e7c26f6c536ce
BVCAD\pasante_innovacion <pasanteinnovacion@bvc.com.co>
2018-02-21 09:10:37 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Form,FormGroup,FormControl, ControlLabel, Button} from 'react-bootstrap'
+
+class App extends Component {
+  render() {
+    return (
+      <Form inline className="Hola">
+  <FormGroup controlId="formInlineName">
+    <ControlLabel>Name</ControlLabel>{' '}
+    <FormControl type="text" placeholder="German Sanchez" />
+  </FormGroup>{' '}
+  <FormGroup controlId="formInlineEmail">
+    <ControlLabel>Email</ControlLabel>{' '}
+    <FormControl type="email" placeholder="pasante@bvc.com" />
+  </FormGroup>{' '}
+  <Button type="submit">Send invitation</Button>
+</Form>
+    );
+  }
+}
+
+export default App;
